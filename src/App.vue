7155543a50a33740a4d6504d5ddbdf39f5157dc7
cromeo98 <romeo.christian98@gmail.com>
2021-07-13 23:26:08 +0200

    call api

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <Header msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+
+import Header from '@/components/Header.vue';
+import Main from '@/components/Main.vue';
 
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
 
 <style lang="scss">
 
-@import "@/style/general.scss"
+@import "@/style/general.scss";
 
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="jumbo">
+    <input type="text" name="search-movie" id="search-movie">
+    <button type="button" class="btn btn-outline-primary">Primary</button>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "Main",
+    data () {
+        return {
+          apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=d2e20f2e9286826809356fcd801cdf54&query=matrix',
+          movieList: []
+        }
+    },
+    created(){
+      this.getMovies();
+    },
+    methods: {
+      getMovies(){
+        axios
+          .get(this.apiURL)
+          .then(res => {
+            this.movieList = res.data;
+            console.log(this.movieList);
+          })
+      }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>

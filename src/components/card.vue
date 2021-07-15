772a1ccb9edc53fe599f1e4e223a7cf22d7180a7
cromeo98 <romeo.christian98@gmail.com>
2021-07-15 05:58:48 +0200

    end milestone 2

--- src/components/card.vue
+++ src/components/card.vue
@@ -5,7 +5,16 @@
           <!-- OR <li>Titolo: {{!details.title ? details.name : details.title}}</li>  -->
           <li>Titolo originale: {{details.original_title || details.original_name}}</li>
           <li>Voto: {{details.vote_average}}</li>
-          <li>Lingua: {{details.original_language}}</li>
+          <li v-if="!flags.includes(details.original_language)">
+              Lingua: {{details.original_language}}
+          </li>
+          <li v-else>
+              Lingua: 
+              <img :src="
+                        require(`../assets/${details.original_language}.png`)
+                       " 
+                   :alt="`${details.original_language} flag`">
+          </li>
       </ul>    
   </div>
 </template>
@@ -13,12 +22,23 @@
 <script>
 export default {
     name: 'Card',
+    data(){
+        return{
+            flags: ['en', 'it']
+        }
+    },
     props: {
         details: Object
     }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+
+li{
+    img{
+        width: 15px;
+    }
+}
 
 </style>
